679a9f63837f6463a93753f1d262af74e1ac1f17
Yaroslaff Fedin <me@inviz.ru>
2009-04-03 12:53:41 +0700

    Merged new art

---
+++ Ext/Widgets/Traits/Draggable.js
@@ -0,0 +1,18 @@
+ART.Widget.Window.Traits.Draggable = new Class({
+	options: {
+		draggable: true
+	},
+	
+	initialize: function(options) {
+		this.parent(options)
+		if (this.options.draggable) this.makeDraggable()
+	},
+
+	makeDraggable: function(){
+		if (this.madeDraggable) return;
+		this.madeDraggable = true;
+
+		new Drag.Move(this.element, {handle: [this.header, this.footer]});
+	}
+})
+
